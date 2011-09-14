415f5f51333a8225a2ed7d6325e488a737ba375f
Juergen Schulze <juergens@met.no>
2011-09-14 10:06:03 +0000

    reading climate data from an url interface - first version

--- ptHistogramElement.cc
+++ ptHistogramElement.cc
@@ -1,35 +1,33 @@
 /*
-  libpets2 - presentation and editing of time series
+ libpets2 - presentation and editing of time series
 
-  $Id$
+ $Id$
 
-  Copyright (C) 2006 met.no
+ Copyright (C) 2006 met.no
 
-  Contact information:
-  Norwegian Meteorological Institute
-  Box 43 Blindern
-  0313 OSLO
-  NORWAY
-  email: diana@met.no
+ Contact information:
+ Norwegian Meteorological Institute
+ Box 43 Blindern
+ 0313 OSLO
+ NORWAY
+ email: diana@met.no
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 // ptHistogramElement.cc : Definitions for HistogramElement class
-
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -45,69 +43,71 @@
 using namespace miutil;
 
 HistogramElement::HistogramElement(const DataSpec cds,
-				   const ptVertFieldf& field,
-				   const Layout& layout,
-				   XAxisInfo* xtime,
-				   vector<miTime>& timeLine)
-  :dataPlotElement(cds, layout, field, xtime),
-   style(layout.fillstyle),
-   lineWidth(layout.lineWidth),deltaY(field.y2-startY-layout.labelSpace),
-   unit(layout.unit),hstart(layout.histStart),hstop(layout.histStop),
-   drawlabel(layout.label)
+    const ptVertFieldf& field, const Layout& layout, XAxisInfo* xtime,
+    vector<miTime>& timeLine) :
+    dataPlotElement(cds, layout, field, xtime), style(layout.fillstyle), lineWidth(
+        layout.lineWidth), deltaY(field.y2 - startY - layout.labelSpace), unit(
+        layout.unit), hstart(layout.histStart), hstop(layout.histStop), drawlabel(
+        layout.label)
 {
 #ifdef DEBUG
   cout << "Inside HistogramElement's constructor" << endl;
 #endif
-  type=HIST;
+  type = HIST;
   deltaY -= YSPACE;
 
-  //make array of time differences
+
+//make array of time differences
   int nt = xtime->xcoord.size();
   deltaT = new int[nt];
 
-  //find scaling of boxes. 12mm is default maximum if delta T = 3
+//find scaling of boxes. 12mm is default maximum if delta T = 3
   max = 12.0;
-  int j= datastart();
-  for (int i = 1; i<nt; i++) {
+  int j = datastart();
+
+  for (int i = 1; i < nt; i++) {
     if (valid(i)) {
-      deltaT[i] = miTime::hourDiff(timeLine[i],timeLine[j]);
+      deltaT[i] = miTime::hourDiff(timeLine[i], timeLine[j]);
 #ifdef DEBUG
-      cout << "Histogram value:" << dval(j)
-	   << " deltaT:" << deltaT[i] << endl;
+      cout << "Times: " << timeLine[i] << " - " << timeLine[j] << "  Histogram value:" << dval(j) << " deltaT:" << deltaT[i] << endl;
 #endif
-      if (dval(j) > deltaT[i]*max/3.0)
-	max = dval(j)*3.0/(deltaT[i]*1.0);
+      if (dval(j) > deltaT[i] * max / 3.0)
+        max = dval(j) * 3.0 / (deltaT[i] * 1.0);
       j++;
     }
   }
-  // hack..don't know timedifference for first timepoint
-  if (nt > 1) deltaT[0]= deltaT[1];
-  else deltaT[0]= 1;
+// hack..don't know timedifference for first timepoint
+  if (nt > 1)
+    deltaT[0] = deltaT[1];
+  else
+    deltaT[0] = 1;
 #ifdef DEBUG
   cout << "Histogram constructor: max:"<<max<<endl;
   cout << "deltaT:"<<endl;
-  for (int i=0; i<nt;i++) cout<<deltaT[i]<<endl;
+  for (int i=0; i<nt;i++)
+    cout<<" DELTAT["<<i<<"] = " << deltaT[i]<<endl;
 #endif
+  cout << "------------------------------------ end histogram constructor\n\n\n";
 }
 
 HistogramElement::~HistogramElement()
 {
-  delete [] deltaT;
+  delete[] deltaT;
 }
 
 // Currently assuming only one histogramelement (should use numHists to control
 // plotting)
 void HistogramElement::plot()
 {
-  if(enabled && visible) {
+  if (enabled && visible) {
 #ifdef DEBUG
     cout << "HistogramElement::plot()" <<endl;
 #endif
-    float th,tw;
-    int i,j;
+    float th, tw;
+    int i, j;
     float y, deltay;
     _prepFont();
-    _getCharSize('0',tw,th);
+    _getCharSize('0', tw, th);
     deltay = deltaY - th;
 
     float DELTAX = tw;
@@ -115,7 +115,7 @@ void HistogramElement::plot()
 
     _setColor(color);
     glLineWidth(lineWidth);
-    if (style != SOLID){
+    if (style != SOLID) {
       glEnable(GL_POLYGON_STIPPLE);
       glPolygonStipple(fillPattern(style));
     }
@@ -124,38 +124,40 @@ void HistogramElement::plot()
     j = datastart();
 
     oldx = xtime->xcoord[startT];
-    for (i=startT; i<=stopT; i++)
+    for (i = startT; i <= stopT; i++)
       if (valid(i)) {
-	newx = xtime->xcoord[i];
-	if (dval(j) >= 0.05) { // don't draw if is ignorable
-	  y = startY + deltay*dval(j)*3/(deltaT[i]*max) ;
+        newx = xtime->xcoord[i];
+        if (dval(j) >= 0.05) { // don't draw if is ignorable
+          y = startY + deltay * dval(j) * 3 / (deltaT[i] * max);
 #ifdef DEBUG
-	  cout << "histogram value:" << dval(j) << " startY:"<<startY
-	       <<" deltaY:"<<deltay<<" deltaT["<<i<<"]:"
-	       <<deltaT[i]<<" max:"<<max
-	       <<" verdi:"<<y<<endl;
+          cout << "histogram value:" << dval(j) << " startY:"<<startY
+          <<" deltaY:"<<deltay<<" deltaT["<<i<<"]:"
+          <<deltaT[i]<<" max:"<<max
+          <<" verdi:"<<y<<endl;
 #endif
-	  float del= (newx-oldx)/10.0;
-	  float x1 = oldx + hstart*del;
-	  float x2 = newx - (10-hstop)*del;
-	  // enclosing box
-	  glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
-	  glBegin(GL_LINE_STRIP);
-	  glVertex2f(x1,startY); glVertex2f(x1,y);
-	  glVertex2f(x2,y); glVertex2f(x2,startY);
-	  glEnd();
-	  glRectf(x1,startY,x2,y);
-	  _updatePrinting();
-	  if (drawlabel){
-	    glDisable(GL_POLYGON_STIPPLE);
-	    sprintf(text, "%2.1f", dval(j));
-	    _printString(text,x1 + (x2-x1)/2 - DELTAX, y + YSPACE);
-	    _updatePrinting();
-	    glEnable(GL_POLYGON_STIPPLE);
-	  }
-	}
-	oldx = newx;
-	j++;
+          float del = (newx - oldx) / 10.0;
+          float x1 = oldx + hstart * del;
+          float x2 = newx - (10 - hstop) * del;
+          // enclosing box
+          glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+          glBegin(GL_LINE_STRIP);
+          glVertex2f(x1, startY);
+          glVertex2f(x1, y);
+          glVertex2f(x2, y);
+          glVertex2f(x2, startY);
+          glEnd();
+          glRectf(x1, startY, x2, y);
+          _updatePrinting();
+          if (drawlabel) {
+            glDisable(GL_POLYGON_STIPPLE);
+            sprintf(text, "%2.1f", dval(j));
+            _printString(text, x1 + (x2 - x1) / 2 - DELTAX, y + YSPACE);
+            _updatePrinting();
+            glEnable(GL_POLYGON_STIPPLE);
+          }
+        }
+        oldx = newx;
+        j++;
       }
     glDisable(GL_POLYGON_STIPPLE);
   }
